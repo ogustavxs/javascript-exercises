c8cab2de3114193841e08c8b2dde072a2529eecf
Gustavo Pessoa <113478928+ogustavxs@users.noreply.github.com>
2024-08-02 12:30:45 -0300

    Exercicio concluido

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = (num1, num2) => {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = (num1, num2) => {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = (array) => {
+	return array.reduce((acc, item) => {
+		return acc + item;
+	}, 0);
 };
 
-const multiply = function() {
-
+const multiply = (array) => {
+	return array.reduce((acc, item) => {
+		return acc * item;
+	}, 1);
 };
 
-const power = function() {
-	
+const power = (num1, num2) => {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = (num) => {
+	if (num < 0) {
+		return 0;
+		// biome-ignore lint/style/noUselessElse: <explanation>
+	} else if (num === 0) {
+		return 1;
+		// biome-ignore lint/style/noUselessElse: <explanation>
+	} else {
+		return num * factorial(num - 1);
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
