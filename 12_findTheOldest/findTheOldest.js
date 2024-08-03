fbbc3210ba580482bd38b3b1327240214c1f5690
Gustavo Pessoa <113478928+ogustavxs@users.noreply.github.com>
2024-08-03 16:32:51 -0300

    exercicio findTheOldest concluido

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (arr) => {
+    const person = arr.reduce((acc, item) => {
+      let age;
+      if (item.yearOfDeath === undefined) {
+        const actualYear = new Date().getFullYear();
+        age = actualYear - item.yearOfBirth;
+      } else {
+        age = item.yearOfDeath - item.yearOfBirth;
+      }
+  
+      let previousAge;
+      if (acc.yearOfDeath === undefined) {
+        const actualYear = new Date().getFullYear();
+        previousAge = actualYear - acc.yearOfBirth;
+      } else {
+        previousAge = acc.yearOfDeath - acc.yearOfBirth;
+      }
+  
+      if (age > previousAge) {
+        return item;
+      }
+      return acc;
+    }, arr[0]);
+    return person;
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
