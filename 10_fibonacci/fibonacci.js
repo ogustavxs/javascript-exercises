988773fb92a2876f7ec43f91705c09cfb94504bb
Gustavo Pessoa <113478928+ogustavxs@users.noreply.github.com>
2024-08-03 14:50:20 -0300

    Exercicio fibonacci concluido

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+    const fibonacciSequence = [0, 1]
+    if (fibonacciSequence.length-1 >= n) {
+		if (n < 0) {
+			return 'OOPS'
+		// biome-ignore lint/style/noUselessElse: <explanation>
+		} else {
+			return fibonacciSequence[n]
+		}
+    // biome-ignore lint/style/noUselessElse: <explanation>
+    } else {
+		for (let i = 1; fibonacciSequence.length-1  <= n; i++) {
+			const penultimoIndex = fibonacciSequence.length-2
+			const ultimoIndex = fibonacciSequence.length-1
+			fibonacciSequence.push((fibonacciSequence[penultimoIndex]+ fibonacciSequence[ultimoIndex]))
+		}
+	}
+	return fibonacciSequence[n]
 };
 
 // Do not edit below this line
